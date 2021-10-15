800f5cf8958673642847630fe646b03cbc5e347b
David Freifeld <freifeld.david@gmail.com>
2021-10-15 15:10:23 -0700

    Module 2 initial progress

--- markov/markov.jl
+++ markov/markov.jl
@@ -1,2 +1,63 @@
-E = [rand(), rand(), rand()]
-for 
+using Printf
+E = [rand()*100, rand()*100, rand()*100j]
+display(E)
+
+F = zeros(size(E,1), size(E,1))
+for i in 1:size(E,1)
+    for j in 1:size(E,1)
+        if E[i] == E[j]
+            F[i,j] = E[i]
+        else
+            F[i,j] = E[i]+E[j]
+        end
+    end
+end
+for i in 1:size(E,1)
+	for j in 1:size(E,1)-i
+        tmp = rand()
+	    F[i, j+i] += tmp
+	    F[j+i, i] += tmp
+    end
+end
+@printf("\nF:\n")
+display(F)
+@printf("\n")
+
+K = 0
+
+vals = []
+for i in 1:size(E,1)
+    sum = 0
+    for j in 1:size(E,1)
+        # @printf("%s %s\n", E[i], F[i,j])
+        sum += exp(E[i] - F[i,j])
+    end
+    append!(vals, sum)
+end
+K = rand() * 1/maximum(vals)
+@printf("K:\n")
+display(K)
+
+@printf("\n");
+
+P = zeros(size(F,1), size(F,2))
+for i in 1:size(P,1)
+    for j in 1:size(P,2)
+        # @printf("i: %s j: %s\n",i,j)
+        # @printf("E[i]: %s, F[i,j]: %s, diff: %s\n", E[i], F[i,j], E[i]-F[i,j])       
+        P[i,j] = K * exp(E[i] - F[i,j])
+    end
+end
+@printf("P:\n")
+display(P)
+@printf("\n")
+
+x = [1,2,3]
+x = transpose(x)
+whee = x*P
+display(whee); @printf("\n");
+for i in 1:10000
+    global whee = whee*P
+end
+display(whee)
+@printf("\n")
--- ntina/ntina.cpp
+++ ntina/ntina.cpp
@@ -1,33 +1,70 @@
 #include <Eigen/Dense>
 #include <iostream>
-
+#include <vector>
+#include <cstdio>
 
 bool ensure_valid(Eigen::MatrixXf ntina) {
     int n = ntina.rows();
     for (int i = 0; i < n; i++) {
-	if (ntina(i,i) == 1) return false;
+        if (ntina(i,i) == 1) return false;
     }
 
     for (int i = 0; i < n-1; i++) {
-	for (int j = 0; j < n-1-i; j++) {	    
-	    if (ntina(i, j+i+1) == 1 && ntina(j+i+1, i) == 1) return false;
-	}
+        for (int j = 0; j < n-1-i; j++) {	    
+            if (ntina(i, j+i+1) == 1 && ntina(j+i+1, i) == 1) return false;
+        }
+    }    
+    return true;
+}
+
+bool ensure_really_valid(Eigen::MatrixXf ntina) {
+    int n = ntina.rows();
+    Eigen::MatrixXf n_p = ntina;
+    std::vector<Eigen::MatrixXf> cache;
+    cache.push_back(ntina);
+    for (int i = 0; i < (n-1); i++) {
+        Eigen::MatrixXf n_i = n_p * ntina;
+        for (int j = 0; j < cache.size(); j++) {
+            if (n_i == cache[j]) {
+                std::cout << "N^" << j << " = " << "N^" << i << "\n";
+                return false;
+            }
+        }
+        cache.push_back(n_i);
+        n_p = n_i;
     }
-    
     return true;
 }
 
 int main() {
-    for (int n = 1; n < 100; n++) {
-	Eigen::MatrixXf ntina(n,n);
-	for (int i = 1; i <= ntina.rows(); i++) {
-	    for (int j = 1; j <= ntina.cols(); j++) {
-		(i==2*j || i==2*(n-j)+1) ? ntina(i-1,j-1) = 1 : ntina(i-1,j-1) = 0;
-	    }
-	}
-	if (ensure_valid(ntina)) {
-	    std::cout << n << ",";
-	} 
+    for (int n = 1; n < 25; n++) {
+        Eigen::MatrixXf ntina(n,n);
+        for (int i = 1; i <= ntina.rows(); i++) {
+            for (int j = 1; j <= ntina.cols(); j++) {
+                (i==2*j || i==2*(n-j)+1) ? ntina(i-1,j-1) = 1 : ntina(i-1,j-1) = 0;
+            }
+        }
+        if (ensure_valid(ntina)) {
+            if (ensure_really_valid(ntina) == false) {
+                std::cout << n << "\n==========\n";
+                printf("   "); for (int i=0; i<n; ++i) printf("%3d", i+1);
+                for (int i=0; i<n; ++i) {
+                    printf("\n%3d", i+1); 
+                    for (int j=0; j<n; ++j) 
+                        printf("%3c", (ntina(i, j) == 0)? '.' : '#');
+                } printf("\n");
+                std::cout << "==========\n";
+                // for (int i = 0; i < ntina.rows(); i++) {
+                //     for (int j = 0; j < ntina.cols(); j++) {
+                //         std::cout << ((ntina(i, j) == 0) ? ". " : "# ");
+                //     }
+                //     std::cout << "\n";
+                // } 
+                // std::cout << ntina << "\n";
+                // std::cout << " (not rly tho)";
+            }
+        } 
     }
     std::cout << "\n";
+    
 }
