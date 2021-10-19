fbb577a464c8ee1c7da132f4081cdf471753d3f8
David Freifeld <freifeld.david@gmail.com>
2021-10-18 21:07:44 -0700

    Finalish markov code

--- markov/markov.jl
+++ markov/markov.jl
@@ -1,63 +1,84 @@
 using Printf
-E = [rand()*100, rand()*100, rand()*100j]
-display(E)
+using LinearAlgebra
+using Plots
 
-F = zeros(size(E,1), size(E,1))
-for i in 1:size(E,1)
-    for j in 1:size(E,1)
+function pprint(matrix)
+    print("\n")
+    display(matrix)
+    print("\n\n")
+end
+
+n = parse(Int64, ARGS[1])
+#E = [0,1,2.1,3,5,6,9.4,8,9,10,10,12,13,14,15]
+E = zeros(n)
+for i in 1:n
+    E[i] = rand()
+end
+#pprint(E)
+
+F = zeros(n, n)
+for i in 1:n
+    for j in 1:n
         if E[i] == E[j]
-            F[i,j] = E[i]
+            F[i,j] = E[i]# +rand()
         else
             F[i,j] = E[i]+E[j]
         end
     end
 end
-for i in 1:size(E,1)
-	for j in 1:size(E,1)-i
+print("F (original):")
+#pprint(F)
+for i in 1:n
+    for j in 1:n-i
         tmp = rand()
-	    F[i, j+i] += tmp
-	    F[j+i, i] += tmp
+	    #F[i, j+i] += tmp
+	    #F[j+i, i] += tmp
     end
 end
-@printf("\nF:\n")
-display(F)
-@printf("\n")
-
-K = 0
+print("F (corrected)")
+#pprint(F)
+K = zeros(n)
 
 vals = []
-for i in 1:size(E,1)
+for i in 1:n
     sum = 0
-    for j in 1:size(E,1)
+    for j in 1:n
         # @printf("%s %s\n", E[i], F[i,j])
         sum += exp(E[i] - F[i,j])
     end
-    append!(vals, sum)
+    # append!(vals, sum)
+    K[i] = 1/sum
 end
-K = rand() * 1/maximum(vals)
-@printf("K:\n")
-display(K)
+print("K: ", K, "\n")
 
-@printf("\n");
-
-P = zeros(size(F,1), size(F,2))
-for i in 1:size(P,1)
-    for j in 1:size(P,2)
-        # @printf("i: %s j: %s\n",i,j)
-        # @printf("E[i]: %s, F[i,j]: %s, diff: %s\n", E[i], F[i,j], E[i]-F[i,j])       
-        P[i,j] = K * exp(E[i] - F[i,j])
+P = zeros(n, n)
+for i in 1:n
+    for j in 1:n
+        P[i,j] = K[i] * exp(E[i] - F[i,j])
     end
 end
-@printf("P:\n")
-display(P)
-@printf("\n")
 
-x = [1,2,3]
-x = transpose(x)
-whee = x*P
-display(whee); @printf("\n");
+print("P:")
+#pprint(P)
+# softmax!(P)
+# pprint(P)
+print("Init:")
+x = zeros(n)
+for i in 1:n
+    x[i] = rand()
+end
+x /= sum(x)
+x_t = zeros(1,n)
+transpose!(x_t, x)
+#pprint(x)
+whee = x_t*P
+display(whee); @printf("\nConverged: \n");
 for i in 1:10000
+    # pprint(whee);
     global whee = whee*P
 end
-display(whee)
-@printf("\n")
+#pprint(whee)
+plot(reshape(whee, length(whee), 1))
+m = maximum(whee)
+plot!(reshape(E*m, length(E), 1))
+gui()
